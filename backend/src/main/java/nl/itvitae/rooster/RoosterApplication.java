5da64d955e8cb994c06a043a447ea449c9195765
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-06-26 10:55:10 +0200

    added spring initialized backend folder

---
+++ backend/src/main/java/nl/itvitae/rooster/RoosterApplication.java
@@ -0,0 +1,13 @@
+package nl.itvitae.rooster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoosterApplication.class, args);
+	}
+
+}
