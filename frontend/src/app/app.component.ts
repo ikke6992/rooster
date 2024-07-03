4140eb8944712117a9eb8e33be06dee8afac8572
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-07-03 14:09:36 +0200

    put the add-group functionality in a modal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { DataService } from './data.service';
 import { AddGroupComponent } from './add-group/add-group.component';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AddGroupComponent],
+  imports: [CommonModule, RouterOutlet, AddGroupComponent, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -20,5 +21,20 @@ export class AppComponent implements OnInit {
     this.dataService.getData().subscribe((response: any[]) => {
       this.data = response;
     });
+  }
+
+  showModal() {
+    let modal_t = document.getElementById('add-group');
+    if (modal_t !== null) {
+      modal_t.classList.remove('hhidden');
+      modal_t.classList.add('sshow');
+    }
+  }
+  closeModal() {
+    let modal_t = document.getElementById('add-group');
+    if (modal_t !== null) {
+      modal_t.classList.remove('sshow');
+      modal_t.classList.add('hhidden');
+    }
   }
 }
