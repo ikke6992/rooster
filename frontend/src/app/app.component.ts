92b17ba66f3fef04f7c1cb3e7d9128f640db7f30
RaafiKuijper <raafi.kuijper@gmail.com>
2024-07-17 14:59:46 +0200

    Move components + routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,42 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { DataService } from './data.service';
-import { AddGroupComponent } from './add-group/add-group.component';
-import { ModalComponent } from './modal/modal.component';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+          
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AddGroupComponent, ModalComponent, ScheduleComponent],
+  imports: [CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = "frontend"
-  data: any[] = [];
 
-  constructor(private dataService: DataService) {}
-
-  ngOnInit(): void {
-    this.dataService.getData().subscribe((response: any[]) => {
-      this.data = response;
-    });
-  }
-
-  showModal() {
-    let modal_t = document.getElementById('add-group');
-    if (modal_t !== null) {
-      modal_t.classList.remove('hhidden');
-      modal_t.classList.add('sshow');
-    }
-  }
-  closeModal() {
-    let modal_t = document.getElementById('add-group');
-    if (modal_t !== null) {
-      modal_t.classList.remove('sshow');
-      modal_t.classList.add('hhidden');
-    }
-  }
 }
+
+
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ViewGroupsComponent } from './view-groups/view-groups.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'schedule', component: ScheduleComponent },
+    { path: 'group', component: ViewGroupsComponent },
+];
