264f319df95999cf2feb5361d384ec49fc5b0d50
RaafiKuijper <raafi.kuijper@gmail.com>
2024-09-02 10:45:29 +0200

    Remove FreeDay backend

--- backend/src/main/java/nl/itvitae/rooster/freeday/FreeDayController.java
+++ backend/src/main/java/nl/itvitae/rooster/freeday/FreeDayController.java
@@ -2,12 +2,15 @@ package nl.itvitae.rooster.freeday;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +36,15 @@ public class FreeDayController {
     }
     URI locationOfFreeDay = ucb.path("/api/v1/groups").buildAndExpand(freeDay.getId()).toUri();
     return ResponseEntity.created(locationOfFreeDay).body(freeDayRepository.save(freeDay));
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> removeFreeDay(@PathVariable Long id){
+    Optional<FreeDay> optFreeDay = freeDayRepository.findById(id);
+    if (optFreeDay.isEmpty()){
+      return ResponseEntity.notFound().build();
+    }
+    freeDayRepository.delete(optFreeDay.get());
+    return ResponseEntity.noContent().build();
   }
 }
