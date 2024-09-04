232e6039d89a380e32510ff662fa1324bc39f228
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-09-04 15:28:07 +0200

    Documentation
    Documentation added to teacher section of preventConflicts to clarify what each section of code does

--- backend/src/main/java/nl/itvitae/rooster/scheduledday/ScheduleddayService.java
+++ backend/src/main/java/nl/itvitae/rooster/scheduledday/ScheduleddayService.java
@@ -76,10 +76,12 @@ public class ScheduleddayService {
         //if teacher can be assigned, find an available date for the lesson and assign the teacher
         if (teacher.isTeachesPracticum() == lesson.isPracticum()) {
           boolean teacherAvailable = false;
+
+          //check whether teacher is already working maximum amount of days this week
           int lessons = 0;
+          int dayOfWeek = date.getDayOfWeek().getValue();
           for (Scheduledday otherScheduledDay : scheduleddayRepository.findByDateBetween(
-              date.minusDays(date.getDayOfWeek().getValue()-1), date.plusDays(5-date.getDayOfWeek().getValue()))
-          ) {
+              date.minusDays(dayOfWeek-1), date.plusDays(5-dayOfWeek))) {
             Teacher otherTeacher = otherScheduledDay.getLesson().getTeacher();
             if (otherTeacher != null && teacher.getId().equals(otherTeacher.getId())) {
               lessons++;
@@ -88,14 +90,20 @@ public class ScheduleddayService {
           if (lessons >= teacher.getMaxDaysPerWeek()) {
             break;
           }
+          
+          //check whether teacher is available to work this day
           days: for (MyDay day : teacher.getAvailability()) {
             if (day.getDay().equals(date.getDayOfWeek())) {
+
+              //check if teacher is already teaching another lesson this day
               for (Scheduledday otherScheduledDay : scheduleddayRepository.findByDate(scheduledday.getDate())) {
                 Teacher otherTeacher = otherScheduledDay.getLesson().getTeacher();
                 if (otherTeacher != null && teacher.getId().equals(otherTeacher.getId())) {
                   break days;
                 }
               }
+
+              //assign teacher to this lesson
               lesson.setTeacher(teacher);
               lessonRepository.save(lesson);
               teacherAvailable = true;
@@ -103,6 +111,7 @@ public class ScheduleddayService {
             }
           }
 
+          //if teacher isn't available and it's not the end of the week, check the next day
           if (!teacherAvailable && date.getDayOfWeek() != DayOfWeek.FRIDAY) {
             scheduledday.setDate(date.plusDays(1));
             preventConflicts(scheduledday, looped);
