391217853254e0e36c6ec4333dbaa31ee24d45ac
RaafiKuijper <raafi.kuijper@gmail.com>
2024-10-02 14:01:19 +0200

    add freeday error message

--- frontend/src/app/free-days/add-free-days/add-free-days.component.ts
+++ frontend/src/app/free-days/add-free-days/add-free-days.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { DataService } from '../data-service';
 import { CommonModule } from '@angular/common';
 import Holidays, { HolidaysTypes } from 'date-holidays';
+import { ModalComponent } from '../../modal/modal.component';
 
 @Component({
   selector: 'app-add-free-days',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, ModalComponent],
   templateUrl: './add-free-days.component.html',
   styleUrl: './add-free-days.component.css',
 })
@@ -22,23 +29,26 @@ export class AddFreeDaysComponent {
     date: new FormControl(''),
   });
 
-  addMultipleFreedays: FormGroup
+  addMultipleFreedays: FormGroup;
+  errorMsg!: string;
 
   constructor(private dataService: DataService, private fb: FormBuilder) {
     this.holidays = this.hd
-    .getHolidays(2025)
-    .filter(
-      (holiday) =>
-        holiday.type == 'public' ||
-        holiday.name == 'Bevrijdingsdag' ||
-        holiday.name == 'Goede Vrijdag'
-    );
+      .getHolidays(2025)
+      .filter(
+        (holiday) =>
+          holiday.type == 'public' ||
+          holiday.name == 'Bevrijdingsdag' ||
+          holiday.name == 'Goede Vrijdag'
+      );
     this.addMultipleFreedays = this.fb.group({
       year: 2024,
-      array: this.fb.array(this.holidays.map(holiday => new FormControl(false)))
+      array: this.fb.array(
+        this.holidays.map((holiday) => new FormControl(false))
+      ),
     });
   }
-  
+
   get checkboxesArray(): FormArray {
     return this.addMultipleFreedays.get('array') as FormArray;
   }
@@ -53,12 +63,14 @@ export class AddFreeDaysComponent {
       },
       (error) => {
         console.error('Error:', error);
+        this.showModal('error');
+        this.errorMsg = error.error;
       }
     );
   }
 
   onSubmit2() {
-    const data = this.addMultipleFreedays .value;
+    const data = this.addMultipleFreedays.value;
     console.log(data);
     this.dataService.addMultipleDays(data).subscribe(
       (response) => {
@@ -69,5 +81,20 @@ export class AddFreeDaysComponent {
         console.error('Error:', error);
       }
     );
+  }
+
+  showModal(name: string) {
+    let modal_t = document.getElementById(name);
+    if (modal_t !== null) {
+      modal_t.classList.remove('hhidden');
+      modal_t.classList.add('sshow');
+    }
+  }
+  closeModal(name: string) {
+    let modal_t = document.getElementById(name);
+    if (modal_t !== null) {
+      modal_t.classList.remove('sshow');
+      modal_t.classList.add('hhidden');
+    }
   }
 }
