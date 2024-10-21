6af34ff2d582eb7d6e2f0f6304875b0a646e5fe9
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-10-21 13:36:55 +0200

    days and weeks limited
    Adapted amount of days for field so that it's between 1 and 5. Adapted amount of weeks for groups and vacations so that it's at least 1.

--- backend/src/main/java/nl/itvitae/rooster/field/FieldController.java
+++ backend/src/main/java/nl/itvitae/rooster/field/FieldController.java
@@ -33,10 +33,14 @@ public class FieldController {
   public ResponseEntity<?> addField(@RequestBody FieldRequest request, UriComponentsBuilder ucb) {
     if (fieldRepository.findByName(request.name()).isPresent()) {
       return ResponseEntity.status(HttpStatus.CONFLICT).body("Field " + request.name() + " already exists.");
+    } else if (request.daysPhase1() < 1 || request.daysPhase1() > 5 || request.daysPhase2() < 1
+        || request.daysPhase2() > 5 || request.daysPhase3() < 1 || request.daysPhase3() > 5) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount of days needs to be between 1 and 5");
+    } else {
+      final Field field = fieldService.addField(request.name(), request.daysPhase1(), request.daysPhase2(), request.daysPhase3());
+      URI locationOfField = ucb.path("/api/v1/fields").buildAndExpand(field.getId()).toUri();
+      return ResponseEntity.created(locationOfField).body(field);
     }
-    final Field field = fieldService.addField(request.name(), request.daysPhase1(), request.daysPhase2(), request.daysPhase3());
-    URI locationOfField = ucb.path("/api/v1/fields").buildAndExpand(field.getId()).toUri();
-    return ResponseEntity.created(locationOfField).body(field);
   }
 
   @PutMapping("/{id}/edit")
@@ -44,6 +48,9 @@ public class FieldController {
     Optional<Field> field = fieldRepository.findById(id);
     if (field.isEmpty()) {
       return ResponseEntity.badRequest().build();
+    } else if (request.daysPhase1() < 1 || request.daysPhase1() > 5 || request.daysPhase2() < 1
+        || request.daysPhase2() > 5 || request.daysPhase3() < 1 || request.daysPhase3() > 5) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Amount of days needs to be between 1 and 5");
     } else {
       return ResponseEntity.ok(fieldService.editField(field.get(), request.name(), request.daysPhase1(), request.daysPhase2(), request.daysPhase3()));
     }
