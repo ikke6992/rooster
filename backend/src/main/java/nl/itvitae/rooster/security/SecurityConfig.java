4e0c13d798bd7db0ab5ea0224925c6f93086ace4
RaafiKuijper <raafi.kuijper@gmail.com>
2024-10-23 14:26:02 +0200

    Setup JWT backend

--- backend/src/main/java/nl/itvitae/rooster/security/SecurityConfig.java
+++ backend/src/main/java/nl/itvitae/rooster/security/SecurityConfig.java
@@ -1,24 +1,37 @@
 package nl.itvitae.rooster.security;
 
+import lombok.RequiredArgsConstructor;
+import nl.itvitae.rooster.user.MyUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  private final MyUserDetailsService userDetailsService;
+  private final JwtAuthenticationFilter authFilter;
+
   public static final String ROLE_ADMIN = "ADMIN";
 
-  private static final String[] ADMIN_ONLY = {"api/v1/fields/**", "api/v1/groups/**", "api/v1/teachers/**"};
+  private static final String[] ADMIN_ONLY = {"api/v1/fields/**", "api/v1/groups",
+      "api/v1/teachers/**"};
   private static final String[] ADMIN_ONLY_POST = {"api/v1/freedays/**"};
   private static final String[] ADMIN_ONLY_DELETE = {"api/v1/freedays/**"};
   private static final String[] ADMIN_ONLY_PUT = {"api/v1/scheduleddays/**"};
@@ -34,13 +47,31 @@ public class SecurityConfig {
             .requestMatchers(HttpMethod.PUT, ADMIN_ONLY_PUT).hasRole(ROLE_ADMIN)
             .requestMatchers(ADMIN_ONLY).hasRole(ROLE_ADMIN)
             .anyRequest().permitAll())
-        .httpBasic(Customizer.withDefaults());
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authProvider())
+        .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
+      throws Exception {
+    return authConfig.getAuthenticationManager();
+
+  }
+
+  @Bean
+  public AuthenticationProvider authProvider() {
+    var authprovider = new DaoAuthenticationProvider();
+    authprovider.setUserDetailsService(userDetailsService);
+    authprovider.setPasswordEncoder(passwordEncoder());
+    return authprovider;
   }
 }
 
---
+++ backend/src/main/java/nl/itvitae/rooster/user/UserController.java
@@ -0,0 +1,42 @@
+package nl.itvitae.rooster.user;
+
+import lombok.AllArgsConstructor;
+import nl.itvitae.rooster.security.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:4200")
+@AllArgsConstructor
+@RequestMapping("api/v1/users")
+public class UserController {
+
+  private final AuthenticationManager authenticationManager;
+  private final JwtService jwtService;
+
+  @PostMapping("login")
+  public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    try {
+      Authentication authentication = authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(loginRequest.username(),
+              loginRequest.password()));
+
+      if (!authentication.isAuthenticated()) {
+        return new ResponseEntity<>("invalid username/password", HttpStatus.UNAUTHORIZED);
+      }
+
+      return new ResponseEntity<>(jwtService.generateJwt(loginRequest.username()), HttpStatus.OK);
+    } catch (AuthenticationException authException) {
+      return new ResponseEntity<>("invalid username/password", HttpStatus.UNAUTHORIZED);
+    }
+  }
+}
