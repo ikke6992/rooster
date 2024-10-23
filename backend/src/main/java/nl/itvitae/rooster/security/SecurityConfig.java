2eff71a8d31da78e64727f3e6fb72854bc33c95e
RaafiKuijper <raafi.kuijper@gmail.com>
2024-10-23 11:08:02 +0200

    remove access for non-admins

--- backend/src/main/java/nl/itvitae/rooster/security/SecurityConfig.java
+++ backend/src/main/java/nl/itvitae/rooster/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package nl.itvitae.rooster.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,13 +18,21 @@ public class SecurityConfig {
 
   public static final String ROLE_ADMIN = "ADMIN";
 
+  private static final String[] ADMIN_ONLY = {"api/v1/fields/**", "api/v1/groups/**", "api/v1/teachers/**"};
+  private static final String[] ADMIN_ONLY_POST = {"api/v1/freedays/**"};
+  private static final String[] ADMIN_ONLY_DELETE = {"api/v1/freedays/**"};
+  private static final String[] ADMIN_ONLY_PUT = {"api/v1/scheduleddays/**"};
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .cors(CorsConfigurer::disable)
         .csrf(CsrfConfigurer::disable)
         .authorizeHttpRequests(requests -> requests
-            .requestMatchers("api/v1/fields").hasRole(ROLE_ADMIN)
+            .requestMatchers(HttpMethod.POST, ADMIN_ONLY_POST).hasRole(ROLE_ADMIN)
+            .requestMatchers(HttpMethod.DELETE, ADMIN_ONLY_DELETE).hasRole(ROLE_ADMIN)
+            .requestMatchers(HttpMethod.PUT, ADMIN_ONLY_PUT).hasRole(ROLE_ADMIN)
+            .requestMatchers(ADMIN_ONLY).hasRole(ROLE_ADMIN)
             .anyRequest().permitAll())
         .httpBasic(Customizer.withDefaults());
     return http.build();
