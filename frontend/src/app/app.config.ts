3f8e0bba3e2d1369ec6cdcd728b1633e4a32efa2
RaafiKuijper <raafi.kuijper@gmail.com>
2024-10-25 14:21:12 +0200

    part of adding jwt header to requests maybe

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,44 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpEvent,
+  HttpHandlerFn,
+  HttpRequest,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { routes } from './app.routes';
+import { multicast, Observable } from 'rxjs';
+import { AuthInterceptor } from './auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([loggingInterceptor])),
+    // [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}]
   ],
 };
+
+export function loggingInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+): Observable<HttpEvent<unknown>> {
+  // temp hardcoded token
+  const token: string = "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9BRE1JTiJ9XSwic3ViIjoiYWRtaW4iLCJpYXQiOjE3Mjk4NTczMDAsImV4cCI6MTcyOTg2MDkwMH0.ovfFo-KWaBJtGQ7ig7Zr78Jumc52dq67bSVLEN7GHiA8_tmbig9S5zqHX2MW7-Vi_-9qwPM-8-JvxBiJY5HsWw"
+  if (!token) {
+    return next(req);
+  }
+  
+  const req1 = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`,  // This is now set as a single string
+    },
+  });
+
+  console.log(req1);  
+
+  return next(req1);
+}
+
---
+++ frontend/src/app/auth-interceptor.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpHandlerFn,
+  HttpInterceptor,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    const token = 'eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9BRE1JTiJ9XSwic3ViIjoiYWRtaW4iLCJpYXQiOjE3Mjk4NDgwMzIsImV4cCI6MTcyOTg1MTYzMn0.H7r-vb2AlLNT5FhYg3gHcWtcgHG_by2qJnK-1Banplxa4mXOr8VWdFnLD_LkOe3tb6kTjlqHwsuenGJ1rJE6JQ'
+
+    if (!token) {
+      return next.handle(req);
+    }
+
+    const req1 = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
+    });
+    
+      console.log(req1.headers.get('Authorization'));
+  console.log(token);
+
+    return next.handle(req1);
+  }
+}
+
+export const loggingInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn
+) => {
+  // temp hardcoded token
+  const token: string = 'eyJhbGciOiJIUzUxMiJ9.eyJyb2xlcyI6W3siYXV0aG9yaXR5IjoiUk9MRV9BRE1JTiJ9XSwic3ViIjoiYWRtaW4iLCJpYXQiOjE3Mjk4NTM1MjksImV4cCI6MTcyOTg1NzEyOX0.OsXBNLFK4W2bemaWtff_nhfDW0p6312DEf9vQIxTMmN6p5st0KckKN6CiRPc4LCr7ulDFeWyIogvaj8oi19low'
+  if (!token) {
+    return next(req);
+  }
+  
+  const req1 = req.clone({
+    headers: req.headers.set('Authorization', 'Bearer ' + token),
+  });
+
+  console.log(req1);
+
+  return next(req1);
+}
