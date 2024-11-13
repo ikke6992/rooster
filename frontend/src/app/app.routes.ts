fb61ce2b344fcf5b8af58d249c65978a45a94200
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-11-13 11:23:30 +0100

    loggedInGuard
    Created loggedInGuard to make routes unreachable when not logged in

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ViewGroupsComponent } from './view-groups/view-groups.component';
 import { ViewTeachersComponent } from './view-teachers/view-teachers.component';
 import { FreeDaysComponent } from './free-days/free-days.component';
 import { ViewFieldsComponent } from './view-fields/view-fields.component';
+import { loggedInGuard } from './logged-in.guard';
 
 export const routes: Routes = [
   { path: '', component: ScheduleComponent },
-  { path: 'groups', component: ViewGroupsComponent },
-  { path: 'fields', component: ViewFieldsComponent },
-  { path: 'teachers', component: ViewTeachersComponent },
+  { path: 'groups', component: ViewGroupsComponent, canActivate: [loggedInGuard] },
+  { path: 'fields', component: ViewFieldsComponent, canActivate: [loggedInGuard] },
+  { path: 'teachers', component: ViewTeachersComponent, canActivate: [loggedInGuard] },
   { path: 'free-days', component: FreeDaysComponent },
+  { path: '**', redirectTo: ''}
 ];
