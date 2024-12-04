65c0f62361834cc65f26e35539c0c45a6a310f90
RaafiKuijper <raafi.kuijper@gmail.com>
2024-12-04 12:16:29 +0100

    Change text colour on dark background

--- frontend/src/app/view-groups/view-groups.component.ts
+++ frontend/src/app/view-groups/view-groups.component.ts
@@ -23,6 +23,7 @@ export class ViewGroupsComponent {
   groups: any[] = [];
   fields: any[] = [];
   teachers: any[] = [];
+  BRIGHTNESS_THRESHOLD: number = 128;
 
   constructor(private dataService: DataService) {}
 
@@ -84,5 +85,15 @@ export class ViewGroupsComponent {
       modal_t.classList.remove('sshow');
       modal_t.classList.add('hhidden');
     }
+  }
+
+  calculateBrightness(color: string): boolean{
+    const r = parseInt(color.substring(1, 3), 16);
+    const g = parseInt(color.substring(3, 5), 16);
+    const b = parseInt(color.substring(5, 7), 16);
+
+    const brightness = Math.sqrt(0.299*r*r + 0.587*g*g + 0.114*b*b);    
+    
+    return brightness < this.BRIGHTNESS_THRESHOLD;
   }
 }
