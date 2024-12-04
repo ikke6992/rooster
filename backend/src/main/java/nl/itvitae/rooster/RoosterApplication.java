8fee03fc0ecd2896134f1b59ef0195962b1abbfd
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-12-04 12:38:00 +0100

    backend gui
    Created simple gui so a user can run a jar-file of the program instead of using the console. The program closes when the gui is closed.

--- backend/src/main/java/nl/itvitae/rooster/RoosterApplication.java
+++ backend/src/main/java/nl/itvitae/rooster/RoosterApplication.java
@@ -1,13 +1,31 @@
 package nl.itvitae.rooster;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import javax.swing.*;
+import java.awt.*;
 
 @SpringBootApplication
-public class RoosterApplication {
+public class RoosterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RoosterApplication.class, args);
+		new SpringApplicationBuilder(RoosterApplication.class).headless(false).run(args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		JFrame frame = new JFrame("Rooster");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(300, 300);
+
+		JPanel panel = new JPanel(new BorderLayout());
+		JTextField text = new JTextField("Running...");
+		panel.add(text, BorderLayout.CENTER);
+		frame.setContentPane(panel);
+		frame.setVisible(true);
+	}
 }
