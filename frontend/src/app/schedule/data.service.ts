b7a67a44c98fab01f99a00b2e4fcc5547cf27ec6
RaafiKuijper <raafi.kuijper@gmail.com>
2025-07-02 11:33:22 +0200

    consistent urls (proper way)

--- frontend/src/app/schedule/data.service.ts
+++ frontend/src/app/schedule/data.service.ts
@@ -8,7 +8,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class DataService {
-  private apiUrl = 'http://localhost:8080/api/v1/';
+  private apiUrl = 'http://localhost:8080/api/v1';
 
   constructor(private http: HttpClient) {}
 
@@ -17,14 +17,14 @@ export class DataService {
   }
 
   getScheduledDaysByMonth(month: number, year: number): Observable<any> {
-    const url = `${this.apiUrl}scheduleddays/month/${month}/${year}`
+    const url = `${this.apiUrl}/scheduleddays/month/${month}/${year}`
     return this.http.get<any>(url);
   }
 
   getExcel(): Observable<any> {
-    return this.http.get(this.apiUrl + `scheduleddays/export`, { responseType: 'blob' }).pipe(
+    return this.http.get(this.apiUrl + `/scheduleddays/export`, { responseType: 'blob' }).pipe(
       map((response: Blob) => {
-        saveAs(response, 'scheduleddays.xlsx');
+        saveAs(response, '/scheduleddays.xlsx');
         return response;
       })
     );
@@ -36,34 +36,34 @@ export class DataService {
   }
 
   override(id: number, data: any) {
-    const url = `${this.apiUrl}scheduleddays/override/${id}`;
+    const url = `${this.apiUrl}/scheduleddays/override/${id}`;
     return this.http.put<any>(url, data, {
       headers: new HttpHeaders({ 'Content-type': 'application/json' }),
     });
   }
 
   editNote(id: number, note: any){
-    const url = `${this.apiUrl}lessons/note/${id}`;
+    const url = `${this.apiUrl}/lessons/note/${id}`;
     return this.http.put<any>(url, note, { responseType: 'text' as 'json' });
   }
 
   getGroups(): Observable<any> {
-    const url =  `${this.apiUrl}groups`
+    const url =  `${this.apiUrl}/groups`
     return this.http.get(url);
   }
 
   getTeachers(): Observable<any> {
-    const url =  `${this.apiUrl}teachers`
+    const url =  `${this.apiUrl}/teachers`
     return this.http.get(url);
   }
 
   addLesson(data: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl + 'scheduleddays', data, {
+    return this.http.post<any>(this.apiUrl + '/scheduleddays', data, {
       headers: new HttpHeaders({ 'Content-type': 'application/json' }),
     });
   }
   
   removeLesson(id: number){
-    return this.http.delete<any>(`${this.apiUrl}scheduleddays/${id}`);
+    return this.http.delete<any>(`${this.apiUrl}/scheduleddays/${id}`);
   }
 } 
